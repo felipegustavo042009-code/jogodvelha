1e3cf129dcd68aefab025f67458bfb2183179b5f
Felipe <felipegustavo042009@gmail.com>
2025-10-27 10:30:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -63,50 +63,170 @@ app.get('/teams/brazil', async (req, res) => {
     
     const timesMock = [
       { 
-        team: { 
-          id: 131, 
-          name: 'Corintias', 
-          country: 'Brazil', 
-          founded: 1895,
-          logo: 'https://media.api-sports.io/football/teams/131.png'
-        },
-        venue: { 
-          id: 234, 
-          name: 'Maracanã', 
-          city: 'Rio de Janeiro',
-          image: 'https://media.api-sports.io/football/venues/234.png'
-        }
-      },
-      { 
-        team: { 
-          id: 135, 
-          name: 'Cruzeiro', 
-          country: 'Brazil', 
-          founded: 1930,
-          logo: 'https://media.api-sports.io/football/teams/135.png'
-        },
-        venue: { 
-          id: 235, 
-          name: 'Morumbi', 
-          city: 'São Paulo',
-          image: 'https://media.api-sports.io/football/venues/235.png'
-        }
-      },
-      { 
-        team: { 
-          id: 134, 
-          name: 'Ceara', 
-          country: 'Brazil', 
-          founded: 1914,
-          logo: 'https://media.api-sports.io/football/teams/134.png'
-        },
-        venue: { 
-          id: 236, 
-          name: 'Allianz Parque', 
-          city: 'São Paulo',
-          image: 'https://media.api-sports.io/football/venues/236.png'
-        }
-      }
+    team: { 
+      id: 131, 
+      name: 'Corinthians', 
+      country: 'Brazil', 
+      founded: 1895,
+      logo: 'https://media.api-sports.io/football/teams/131.png'
+    },
+    venue: { 
+      id: 234, 
+      name: 'Maracanã', 
+      city: 'Rio de Janeiro',
+      image: 'https://media.api-sports.io/football/venues/234.png'
+    }
+  },
+  { 
+    team: { 
+      id: 135, 
+      name: 'Cruzeiro', 
+      country: 'Brazil', 
+      founded: 1930,
+      logo: 'https://media.api-sports.io/football/teams/135.png'
+    },
+    venue: { 
+      id: 235, 
+      name: 'Morumbi', 
+      city: 'São Paulo',
+      image: 'https://media.api-sports.io/football/venues/235.png'
+    }
+  },
+  { 
+    team: { 
+      id: 134, 
+      name: 'Ceará', 
+      country: 'Brazil', 
+      founded: 1914,
+      logo: 'https://media.api-sports.io/football/teams/134.png'
+    },
+    venue: { 
+      id: 236, 
+      name: 'Allianz Parque', 
+      city: 'São Paulo',
+      image: 'https://media.api-sports.io/football/venues/236.png'
+    }
+  },
+  { 
+    team: { 
+      id: 136, 
+      name: 'Flamengo', 
+      country: 'Brazil', 
+      founded: 1895,
+      logo: 'https://media.api-sports.io/football/teams/136.png'
+    },
+    venue: { 
+      id: 237, 
+      name: 'Maracanã', 
+      city: 'Rio de Janeiro',
+      image: 'https://media.api-sports.io/football/venues/237.png'
+    }
+  },
+  { 
+    team: { 
+      id: 137, 
+      name: 'Palmeiras', 
+      country: 'Brazil', 
+      founded: 1914,
+      logo: 'https://media.api-sports.io/football/teams/137.png'
+    },
+    venue: { 
+      id: 238, 
+      name: 'Allianz Parque', 
+      city: 'São Paulo',
+      image: 'https://media.api-sports.io/football/venues/238.png'
+    }
+  },
+  { 
+    team: { 
+      id: 138, 
+      name: 'São Paulo', 
+      country: 'Brazil', 
+      founded: 1930,
+      logo: 'https://media.api-sports.io/football/teams/138.png'
+    },
+    venue: { 
+      id: 239, 
+      name: 'Morumbi', 
+      city: 'São Paulo',
+      image: 'https://media.api-sports.io/football/venues/239.png'
+    }
+  },
+  { 
+    team: { 
+      id: 139, 
+      name: 'Grêmio', 
+      country: 'Brazil', 
+      founded: 1903,
+      logo: 'https://media.api-sports.io/football/teams/139.png'
+    },
+    venue: { 
+      id: 240, 
+      name: 'Arena do Grêmio', 
+      city: 'Porto Alegre',
+      image: 'https://media.api-sports.io/football/venues/240.png'
+    }
+  },
+  { 
+    team: { 
+      id: 140, 
+      name: 'Internacional', 
+      country: 'Brazil', 
+      founded: 1909,
+      logo: 'https://media.api-sports.io/football/teams/140.png'
+    },
+    venue: { 
+      id: 241, 
+      name: 'Beira-Rio', 
+      city: 'Porto Alegre',
+      image: 'https://media.api-sports.io/football/venues/241.png'
+    }
+  },
+  { 
+    team: { 
+      id: 141, 
+      name: 'Vasco da Gama', 
+      country: 'Brazil', 
+      founded: 1898,
+      logo: 'https://media.api-sports.io/football/teams/141.png'
+    },
+    venue: { 
+      id: 242, 
+      name: 'São Januário', 
+      city: 'Rio de Janeiro',
+      image: 'https://media.api-sports.io/football/venues/242.png'
+    }
+  },
+  { 
+    team: { 
+      id: 142, 
+      name: 'Atlético Mineiro', 
+      country: 'Brazil', 
+      founded: 1908,
+      logo: 'https://media.api-sports.io/football/teams/142.png'
+    },
+    venue: { 
+      id: 243, 
+      name: 'Mineirão', 
+      city: 'Belo Horizonte',
+      image: 'https://media.api-sports.io/football/venues/243.png'
+    }
+  },
+  { 
+    team: { 
+      id: 143, 
+      name: 'Bahia', 
+      country: 'Brazil', 
+      founded: 1931,
+      logo: 'https://media.api-sports.io/football/teams/143.png'
+    },
+    venue: { 
+      id: 244, 
+      name: 'Fonte Nova', 
+      city: 'Salvador',
+      image: 'https://media.api-sports.io/football/venues/244.png'
+    }
+  }
     ];
     
     res.json(timesMock);
@@ -133,3 +253,4 @@ app.listen(PORT, () => {
   console.log(`📍 React App: http://localhost:${PORT}/`);
 
 });
+
